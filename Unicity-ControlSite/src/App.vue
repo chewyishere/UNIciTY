801ad0344170b62e45f22c7253f20ae0cce13380
Chewy Wu <wasinfall@gmail.com>
2018-08-23 18:13:06 -0400

    init update with unity interface and vue UI

---
+++ Unicity-ControlSite/src/App.vue
@@ -0,0 +1,119 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.jpg">
+    <index></index>
+  </div>
+</template>
+
+<script>
+import index from './components/Index'
+
+
+
+export default {
+  components: {
+    index
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 10px;
+}
+
+img{
+    width: 100%;
+    position: fixed;
+    z-index: -1;
+    left: 0;
+}
+
+  h1 {
+    font-weight: normal;
+    font-size: 3em;
+    color: red;
+    text-transform: capitalize;
+    font-family: Impact;
+    background: linear-gradient(to right, orange , yellow, green, cyan, blue, violet);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  }
+
+  
+  h2 {
+    display: inline-block;
+    font-family: futura;
+    font-size: 2em;
+    font-style: italic;
+    color: white;
+  }
+  
+  .block {
+    width: 50%;
+    height: 400px;
+    float: left;
+    display: flex;
+    flex-direction: column;
+  }
+  
+  .text-input, button {
+    width: 70%;
+    height: 30px;
+    text-align: center;
+    border: gray 1px solid;
+  }
+
+  button{
+      width: 500px;
+      height: 100px;
+      font-weight: normal;
+     font-size: 3em;
+    text-transform: capitalize;
+    font-family: Impact;
+     text-shadow: -1px -1px 0 rgba(0,0,0,0.3);
+     font-weight:bold; 
+     color: #FFFFFF;
+     background-color: #A5B8DA; 
+     background-image: linear-gradient(to right, orange , yellow, green, cyan, blue, violet);
+
+  }
+
+
+  button:hover{
+      background-image: linear-gradient(to right, violet, blue, yellow, green, cyan,orange );
+      border: 2px solid white;
+      color: black;
+  }
+
+
+  input{
+      font-size: 16px;
+      margin: 20px auto;
+  }
+
+.block-style{
+    color: white;
+    width: 50%;
+    text-align: center;
+    margin: 0 auto;
+    position: relative;
+    flex-direction: row;
+    align-items: center;
+    align-content: center;
+    height: 150px;
+    float: unset;
+}
+
+label{
+      font-size: 1.5rem;
+    font-weight: 400;
+    margin-left: -80px;
+}
+
+  </style>
---
+++ Unicity-ControlSite/src/components/Index.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="main">
+    <h1>{{ title }}</h1>
+    <div class="block">
+    <h2> YOUTUBE LINKS </h2>
+      <input class="text-input" 
+        :value="songURL"
+        @input="songURL = $event.target.value"
+        placeholder= 'drop your favorite youtube song here'>
+        <input class="text-input" 
+        :value="cookingURL"
+        @input="cookingURL = $event.target.value"
+        placeholder= 'another link, maybe cooking ?'>
+        <input class="text-input" 
+        :value="otherURL"
+        @input="otherURL = $event.target.value"
+        placeholder= 'just drop another one here, anything you like'>
+        </div>
+      <div class="block">
+       <h2> TEXTS </h2>
+        <input 
+        class="text-input" 
+        :value="userData.name"
+        @input="userData.name = $event.target.value" placeholder="your name">
+        <input 
+        class="text-input" 
+        :value="userData.friend"
+        @input="userData.friend = $event.target.value" placeholder="your best friend's name">
+        <input 
+        class="text-input" 
+        :value="userData.mom"
+        @input="userData.mom = $event.target.value" placeholder="your mom's name">
+        <input 
+        class="text-input" 
+        :value="userData.pet"
+        @input="userData.pet = $event.target.value" placeholder="your pet's name">
+    </div>
+    <button v-on:click="submitData">SEND TO DISCO CITY</button>
+  </div>
+</template>
+
+</div>
+
+<script>
+
+  export default {
+    data() {
+      return {
+        title: 'Trip to UNIciTY',
+        songURL:'',
+        cookingURL:'',
+        otherURL:'',
+        userData: {
+          songID: '',
+          cookingID:'',
+          otherID:'',
+          name: '',
+          friend:'',
+          mom:'',
+          pet:'',
+        },
+      }
+    },
+    methods: {
+      parseYoutube(url){
+          var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+          var match = url.match(regExp);
+          return (match&&match[7].length==11)? match[7] : false;
+      },
+
+      submitData() { 
+         this.userData.songID = this.parseYoutube(this.songURL);
+         this.userData.cookingID = this.parseYoutube(this.cookingURL);
+         this.userData.otherID = this.parseYoutube(this.otherURL);
+
+         console.log(this.userData);
+
+         this.$http.put('https://ardiscocity.firebaseio.com/data.json', this.userData)
+         .then(response => {
+
+
+          console.log(response);
+        }, error => {
+          console.log(error);
+        });
+      }
+    }
+  }
+</script>
+
